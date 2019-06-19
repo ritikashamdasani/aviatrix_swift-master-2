a6cf0f9a89a9d2f4221e9fbd6faa271147833221
Apple <apple@Apples-MacBook-Pro-62.local>
2019-06-19 12:07:29 -0400

    Initial commit

---
+++ AviatrixApp/Aviatrix.swift
@@ -0,0 +1,39 @@
+//
+//  Aviatrix.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+class Aviatrix {
+    var running = false
+    var author : String
+    
+    init(authorName: String){
+        author = authorName //authorName will be stored in author 
+    }
+    
+    func start() -> Bool {
+        running = true
+        return running
+    }
+    
+    func refuel() {
+        
+    }
+    
+    func flyTo(destination : String) {
+        
+    }
+    
+    func distanceTo(target : String) {
+    
+    }
+    
+    func knownDestinations() -> [String] {
+       return ["St. Louis"]
+    }
+}
